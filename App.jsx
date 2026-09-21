c291bff8b6edead5580c29116448721b0909de3b
AidenVang <avang15@charlotte.edu>
2026-09-21 14:45:59 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AboutUs from "./components/AboutUs";
+import ProductList from "./components/ProductList";
+import CartItem from "./components/CartItem";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app-container">
+        <h1>Paradise Nursery</h1>
+        <p>Your one‑stop shop for beautiful plants</p>
+
+        <Link to="/products">
+          <button className="get-started-btn">Get Started</button>
+        </Link>
+      </div>
+
+      {/* Navigation Bar */}
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/cart">Cart</Link>
+      </nav>
+
+      {/* Page Routes */}
+      <Routes>
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<CartItem />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
